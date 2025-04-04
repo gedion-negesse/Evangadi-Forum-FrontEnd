44786b33ca2e0b5889dbe01babe8fb0349b2d1ac
gedion <legessegedion@gmail.com>
2025-04-04 19:27:47 -0400

    deploye

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  // baseURL: "http://localhost:6063/api", 
+  // baseURL: "http://localhost:6063/api",
 
-  baseURL:"https://evangadi-forum-backend-1-wb11.onrender.com/api",
- 
+  //baseURL:"https://evangadi-forum-backend-1-wb11.onrender.com/api",
+  baseURL: "https://evangadi-forum-backend-7xxk.onrender.com/api",
 });
 export default axiosBase;
